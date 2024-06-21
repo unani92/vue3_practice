03aafaf196c3675708a77547665ed317ed375753
Yoonhwan Jung <unani92@naver.com>
2024-06-21 18:03:29 +0900

    pinia 세팅

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -1,8 +1,49 @@
 // Utilities
 import { defineStore } from 'pinia'
 
-export const useAppStore = defineStore('app', {
+export const usePersonalInfoStore = defineStore('personalInfo', {
   state: () => ({
-    //
+    email: '' as string,
+    password: '' as string,
   }),
+  actions: {
+    setPersonalInfo(email: string, password: string) {
+      this.email = email
+      this.password = password
+    },
+  },
+})
+
+export const useDeliveryInfoStore = defineStore('deliveryInfo', {
+  state: () => ({
+    name: '' as string,
+    phoneNumber: '' as string,
+    address: {} as {
+      zipCode: string
+      address: string
+      detailAddress?: string
+    },
+  }),
+  actions: {
+    setDeliveryInfo(
+      name: string,
+      phoneNumber: string,
+      address: { zipCode: string; address: string; detailAddress?: string }
+    ) {
+      this.name = name
+      this.phoneNumber = phoneNumber
+      this.address = address
+    },
+  },
+})
+
+export const usePaymentInfoStore = defineStore('paymentInfo', {
+  state: () => ({
+    cardNumber: '' as string,
+  }),
+  actions: {
+    setPaymentInfo(cardNumber: string) {
+      this.cardNumber = cardNumber
+    },
+  },
 })
