952f72170e78deb694e6105acc3fd88eb8cbb40c
Yoonhwan Jung <unani92@naver.com>
2024-06-21 23:14:50 +0900

    navigation guard 세팅

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,10 +7,34 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router/auto'
 import { setupLayouts } from 'virtual:generated-layouts'
+import { useDeliveryInfoStore, usePersonalInfoStore } from '@/stores/app'
+import { storeToRefs } from 'pinia'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   extendRoutes: setupLayouts,
+})
+
+router.beforeEach((to, from, next) => {
+  // Add your custom logic here
+  const personalInfoStore = usePersonalInfoStore()
+  const deliveryInfoStore = useDeliveryInfoStore()
+  const { email, password } = storeToRefs(personalInfoStore)
+  const { name, phoneNumber, address } = storeToRefs(deliveryInfoStore)
+  if (to.path === '/delivery-info') {
+    if (!(email.value && password.value)) {
+      next('/')
+    }
+  }
+  if (to.path === '/payment-info') {
+    if (!(email.value && password.value)) {
+      next('/')
+    }
+    if (!(name.value && phoneNumber.value && address.value)) {
+      next('/delivery-info')
+    }
+  }
+  next()
 })
 
 export default router
