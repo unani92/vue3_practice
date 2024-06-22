f3ad82cedb5268e3cf0a05fac9c8753abe4935b7
Yoonhwan Jung <unani92@naver.com>
2024-06-22 18:58:46 +0900

    feat: 선행 기입사항 미기재시 해당 페이지로 이동 nav guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,7 +7,7 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router/auto'
 import { setupLayouts } from 'virtual:generated-layouts'
-import { useDeliveryInfoStore, usePersonalInfoStore } from '@/stores/app'
+import { useDeliveryInfoStore, usePaymentInfoStore, usePersonalInfoStore } from '@/stores/app'
 import { storeToRefs } from 'pinia'
 
 const router = createRouter({
@@ -19,8 +19,10 @@ router.beforeEach((to, from, next) => {
   // Add your custom logic here
   const personalInfoStore = usePersonalInfoStore()
   const deliveryInfoStore = useDeliveryInfoStore()
+  const paymentInfoStore = usePaymentInfoStore()
   const { email, password } = storeToRefs(personalInfoStore)
   const { name, phoneNumber, address } = storeToRefs(deliveryInfoStore)
+  const { cardNumber } = storeToRefs(paymentInfoStore)
   if (to.path === '/delivery-info') {
     if (!(email.value && password.value)) {
       next('/')
@@ -32,6 +34,17 @@ router.beforeEach((to, from, next) => {
     }
     if (!(name.value && phoneNumber.value && address.value)) {
       next('/delivery-info')
+    }
+  }
+  if (to.path === '/submit-complete') {
+    if (!(email.value && password.value)) {
+      next('/')
+    }
+    if (!(name.value && phoneNumber.value && address.value)) {
+      next('/delivery-info')
+    }
+    if (!cardNumber.value) {
+      next('/payment-info')
     }
   }
   next()
